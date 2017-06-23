96673d706374334eed9e4fadf4f9eb88aed9a90e
stayhunger <richard.tli@gmail.com>
2017-06-23 15:02:31 -0400

    add both residential + condo active listings into one table

--- src/main/java/com/whereq/realtor/batch/ActiveListingRunner.java
+++ src/main/java/com/whereq/realtor/batch/ActiveListingRunner.java
@@ -18,6 +18,8 @@ import org.springframework.stereotype.Component;
 import com.google.common.collect.Lists;
 import com.whereq.realtor.batch.domain.ListingActivePO;
 import com.whereq.realtor.batch.repository.ListingActiveRepository;
+import com.whereq.realtor.xml.bind.LiteCondoProperty;
+import com.whereq.realtor.xml.bind.LiteCondoPropertyWrapper;
 import com.whereq.realtor.xml.bind.LiteResProperty;
 import com.whereq.realtor.xml.bind.LiteResPropertyWrapper;
 
@@ -38,13 +40,13 @@ public class ActiveListingRunner {
 		System.out.println("Removing all active listing first, size: " + repository.count());
 		repository.deleteAll();
 		
-		
+		//scan active resedentials
         JAXBContext jc = JAXBContext.newInstance(LiteResPropertyWrapper.class);
 
         XMLInputFactory xif = XMLInputFactory.newFactory();
         //xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-        XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/active_freehold.xml"));
-        //XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/crea/treb_feed/active/active_freehold.xml"));
+        //XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/active_freehold.xml"));
+        XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/crea/treb_feed/active/active_freehold.xml"));
 
         Unmarshaller unmarshaller = jc.createUnmarshaller();
         LiteResPropertyWrapper  response = (LiteResPropertyWrapper) unmarshaller.unmarshal(xsr );
@@ -55,7 +57,7 @@ public class ActiveListingRunner {
 				
 				if (repository.findByMLS(liteResPro.getMls()) == null)
 				{
-					poList.add(saveIntoListingActiveTable(liteResPro)) ;
+					poList.add(saveIntoListingActiveTable(liteResPro,"RES")) ;
 					logger.info(++i+ ":"+liteResPro.toString());
 				} 
 				else
@@ -66,24 +68,68 @@ public class ActiveListingRunner {
 		
 		logger.info("Total: "+ i + " inserted on residential properties!");
 		
+		
+		//scan active condos
+        jc = JAXBContext.newInstance(LiteCondoPropertyWrapper.class);
+
+        unmarshaller = jc.createUnmarshaller();
+        xif = XMLInputFactory.newFactory();
+        xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/crea/treb_feed/active/active_condo.xml"));
+
+        LiteCondoPropertyWrapper  responseCondo = (LiteCondoPropertyWrapper) unmarshaller.unmarshal(xsr );
+        List<LiteCondoProperty> actCndListings = responseCondo.getActiveProperties();
+        int j=0;
+		for (LiteCondoProperty liteCndPro : actCndListings) {
+			{
+				
+				if (repository.findByMLS(liteCndPro.getMls()) == null)
+				{
+					poList.add(saveIntoListingActiveTable(liteCndPro,"CND")) ;
+					logger.info(++j+ ":"+liteCndPro.toString());
+				} 
+				else
+					logger.info(liteCndPro.getMls() + " exists in DB already !");
+
+			}	
+		}
+		
+		logger.info("Total: "+ j + " inserted on condo properties!");
+		logger.info("Total: "+ (i+j) + " inserted on all active properties!");
+		
 		repository.save(poList);
 		
 	}
 	
 	
-	private ListingActivePO saveIntoListingActiveTable(LiteResProperty litResPro)
+	private ListingActivePO saveIntoListingActiveTable(LiteResProperty litResPro, String type)
 	{
 		ListingActivePO listingPO = null;
 		
-		System.out.println(litResPro.getMls() + " into listing_active table:");
+		System.out.println(litResPro.getMls() + " into listing_active table: for residentila");
 		listingPO = new ListingActivePO();
 		listingPO.setMLS(litResPro.getMls());
 		listingPO.setStatus(litResPro.getStatus());
-		listingPO.setType("RES");
+		listingPO.setType(type);
 		
 		return listingPO;
 		
 	}
+	
+	private ListingActivePO saveIntoListingActiveTable(LiteCondoProperty litCndPro, String type)
+	{
+		ListingActivePO listingPO = null;
+		
+		System.out.println(litCndPro.getMls() + " into listing_active table: for condo");
+		listingPO = new ListingActivePO();
+		listingPO.setMLS(litCndPro.getMls());
+		listingPO.setStatus(litCndPro.getStatus());
+		listingPO.setType(type);
+		
+		return listingPO;
+		
+	}
+	
+	
 	
 	
 }
