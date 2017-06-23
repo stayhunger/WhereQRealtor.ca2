0e9dde6b672d6e1748e73f99eab721a9bb9bffdb
stayhunger <richard.tli@gmail.com>
2017-06-23 13:25:41 -0400

    add slf4j path and use TREB_HOME

--- src/main/java/com/whereq/realtor/batch/ActiveListingRunner.java
+++ src/main/java/com/whereq/realtor/batch/ActiveListingRunner.java
@@ -11,19 +11,16 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.stream.StreamSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Lists;
-import com.whereq.realtor.batch.domain.FH_ListingAddrPO;
-import com.whereq.realtor.batch.domain.FH_ListingExtraPO;
-import com.whereq.realtor.batch.domain.FH_PropertyDetailsPO;
-import com.whereq.realtor.batch.domain.FH_PropertyPO;
-import com.whereq.realtor.batch.domain.FH_PropertyRoomsPO;
-import com.whereq.realtor.batch.domain.ListingFullPO;
 import com.whereq.realtor.batch.domain.ListingActivePO;
 import com.whereq.realtor.batch.repository.ListingActiveRepository;
 import com.whereq.realtor.xml.bind.LiteResProperty;
 import com.whereq.realtor.xml.bind.LiteResPropertyWrapper;
+
 
 @Component(value="active_runner")
 public class ActiveListingRunner {
@@ -34,6 +31,8 @@ public class ActiveListingRunner {
 	
 	public void run() throws JAXBException, XMLStreamException{
 		
+		 Logger logger = LoggerFactory.getLogger(ActiveListingRunner.class);	
+		
 		List<ListingActivePO> poList = Lists.newArrayList();
 		
 		System.out.println("Removing all active listing first, size: " + repository.count());
@@ -43,9 +42,9 @@ public class ActiveListingRunner {
         JAXBContext jc = JAXBContext.newInstance(LiteResPropertyWrapper.class);
 
         XMLInputFactory xif = XMLInputFactory.newFactory();
-        xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-        //XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/active_freehold.xml"));
-        XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/crea/treb_feed/active/active_freehold3.xml"));
+        //xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+        XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/active_freehold.xml"));
+        //XMLStreamReader xsr = xif.createXMLStreamReader(new StreamSource("C:/tmp/crea/treb_feed/active/active_freehold.xml"));
 
         Unmarshaller unmarshaller = jc.createUnmarshaller();
         LiteResPropertyWrapper  response = (LiteResPropertyWrapper) unmarshaller.unmarshal(xsr );
@@ -57,15 +56,15 @@ public class ActiveListingRunner {
 				if (repository.findByMLS(liteResPro.getMls()) == null)
 				{
 					poList.add(saveIntoListingActiveTable(liteResPro)) ;
-					System.out.println(++i+ ":"+liteResPro.toString());
+					logger.info(++i+ ":"+liteResPro.toString());
 				} 
 				else
-					System.out.println(liteResPro.getMls() + " exists in DB already !");
+					logger.info(liteResPro.getMls() + " exists in DB already !");
 
 			}	
 		}
 		
-		System.out.println("Total: "+ i + " inserted !");
+		logger.info("Total: "+ i + " inserted on residential properties!");
 		
 		repository.save(poList);
 		
--- src/main/java/com/whereq/realtor/batch/domain/ListingActivePO.java
+++ src/main/java/com/whereq/realtor/batch/domain/ListingActivePO.java
@@ -1,12 +1,9 @@
 package com.whereq.realtor.batch.domain;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
